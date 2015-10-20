118206cff54d6a6b7b85e2b70a588f5e1163fe6b
thomasgwatson <thomasgabriel.watson@gmail.com>
2015-10-19 22:04:42 -0700

    finished basic

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ var React = require('react-native')
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -17,21 +18,68 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ]
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json'
+
 var nativeTime = React.createClass({
+  getInitialState: function () {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    }
+  },
+  componentDidMount: function () {
+    this.fetchData()
+  },
+  fetchData: function () {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        })
+      })
+      .done()
+  },
   render: function () {
-    var movie = MOCKED_MOVIES_DATA[0]
+    if (!this.state.loaded) {
+      return this.renderLoadingView()
+    }
+
     return (
-        <View style={styles.container}>
-          <Image
-            source={{uri: movie.posters.thumbnail}}
-            style={styles.thumbnail}
-          />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{movie.title}</Text>
-            <Text style={styles.year}>{movie.year}</Text>
-          </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    )
+  },
+
+  renderLoadingView: function () {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    )
+  },
+
+  renderMovie: function (movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
         </View>
-      )
+      </View>
+    )
   },
 })
 
@@ -41,6 +89,10 @@ var styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  listView: {
+    paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
   thumbnail: {
